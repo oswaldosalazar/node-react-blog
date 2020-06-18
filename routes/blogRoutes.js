f26f94942b97636540c56c93ce97c56980fdd786
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-06-17 18:32:39 -0600

    Started Redis caching implementation

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,34 +1,55 @@
-const mongoose = require('mongoose')
-const authenticate = require('@middleware/authenticate')
+const mongoose = require('mongoose');
+const authenticate = require('@middleware/authenticate');
 
-const Blog = mongoose.model('Blog')
+const Blog = mongoose.model('Blog');
 
 module.exports = app => {
   app.get('/api/blogs/:id', authenticate, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
-    })
-    res.send(blog)
-  })
+    });
+    res.send(blog);
+  });
 
   app.get('/api/blogs', authenticate, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id })
-    res.send(blogs)
-  })
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    // Do we have any cached data in redis related to query?
+    const cachedBlogs = await client.get(req.user.id);
+
+    //If yes, respond to request right away and return
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // If no, respond to request and update cache
+
+    const blogs = await Blog.find({ _user: req.user.id });
+
+    res.send(blogs);
+
+    console.log('SERVING FROM MONGODB');
+    client.set(req.user.id, JSON.stringify(blogs));
+  });
 
   app.post('/api/blogs', authenticate, async (req, res) => {
-    const { title, content } = req.body
+    const { title, content } = req.body;
     const blog = new Blog({
       title,
       content,
       _user: req.user.id
-    })
+    });
     try {
-      await blog.save()
-      res.send(blog)
+      await blog.save();
+      res.send(blog);
     } catch (err) {
-      res.send(400, err)
+      res.send(400, err);
     }
-  })
-}
+  });
+};
